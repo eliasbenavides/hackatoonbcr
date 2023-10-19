d0d6c5aa5bc904eaff1bf4a7d18a22846a5c911d
lexie70 <116680625+lexie70@users.noreply.github.com>
2023-10-19 12:41:19 -0600

    styles bcr

--- src/layouts/SearchUsers.jsx
+++ src/layouts/SearchUsers.jsx
@@ -1,13 +1,12 @@
-import * as React from 'react';
+import * as React from "react";
 import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
 
 const SearchUsers = () => {
-
   const { data } = useDemoData({
-    dataSet: 'Commodity',
+    dataSet: "Commodity",
     rowLength: 100,
     maxColumns: 12,
   });
@@ -21,23 +20,24 @@ const SearchUsers = () => {
         </Link>
       </div>
 
-      <Box sx={{ height: '80vh', width: '100%' }}>
-      <DataGrid
-        {...data}
-        initialState={{
-          ...data.initialState,
-          pagination: { paginationModel: { pageSize: 5 } },
-        }}
-        pageSizeOptions={[5, 10, 25]}
-        sx={{
-          boxShadow: 2,
-          border: 2,
-          borderColor: 'primary.light', '& .MuiDataGrid-cell:hover': {
-          color: 'primary.main',
-          },
-        }}
-      />
-    </Box>
+      <Box sx={{ height: "80vh", width: "100%" }}>
+        <DataGrid
+          {...data}
+          initialState={{
+            ...data.initialState,
+            pagination: { paginationModel: { pageSize: 5 } },
+          }}
+          pageSizeOptions={[5, 10, 25]}
+          sx={{
+            boxShadow: 2,
+            border: 2,
+            borderColor: "primary.light",
+            "& .MuiDataGrid-cell:hover": {
+              color: "primary.main",
+            },
+          }}
+        />
+      </Box>
     </div>
   );
 };
