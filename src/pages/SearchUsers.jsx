c2e3541635e60e5c5d5e839b24ce9bbba98c6130
Juan Jose CS <jconejo@prides.net>
2023-10-19 16:47:13 -0600

    feature: merge dev

--- src/pages/SearchUsers.jsx
+++ src/pages/SearchUsers.jsx
@@ -3,33 +3,27 @@ import { Link } from "react-router-dom";
 import { Box, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { getUsers } from "../services/api";
-import { useDemoData } from "@mui/x-data-grid-generator";
+import "../styles/search-users.css";
 
 const SearchUsers = () => {
   const [tableData, setTableData] = useState([]);
 
-  const { data } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 100,
-    maxColumns: 12,
-  });
-
   const columns = [
     { field: "nombre", headerName: "Nombre", width: 200 },
-    { field: "correo", headerName: "Correo", width: 300 },
-    { field: "edad", headerName: "Edad", width: 200 },
-    { field: "sexo", headerName: "Sexo", width: 200 },
-    { field: "telefono", headerName: "Teléfono", width: 300 },
-    { field: "profesion", headerName: "Profesión", width: 300 },
-    { field: "provincia", headerName: "Provincia", width: 300 },
-    { field: "canton", headerName: "Cantón", width: 300 },
+    { field: "correo", headerName: "Correo", width: 200 },
+    { field: "edad", headerName: "Edad", width: 150 },
+    { field: "sexo", headerName: "Sexo", width: 150 },
+    { field: "telefono", headerName: "Teléfono", width: 200 },
+    { field: "profesion", headerName: "Profesión", width: 200 },
+    { field: "provincia", headerName: "Provincia", width: 200 },
+    { field: "canton", headerName: "Cantón", width: 200 },
   ];
 
   useEffect(() => {
     async function fetchUsers() {
       try {
         const data = await getUsers();
-        setTableData(data);
+        setTableData(data.detalle);
       } catch (error) {
         console.error("Error fetching posts:", error);
       }
@@ -57,13 +51,8 @@ const SearchUsers = () => {
       <Box sx={{ height: "80vh", width: "100%" }}>
         <DataGrid
           rows={tableData}
+          getRowId={(row) => row.telefono}
           columns={columns}
-          {...data}
-          initialState={{
-            ...data.initialState,
-            pagination: { paginationModel: { pageSize: 5 } },
-          }}
-          pageSizeOptions={[5, 10, 25]}
           sx={{
             boxShadow: 2,
             border: 2,
