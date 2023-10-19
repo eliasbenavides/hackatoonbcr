bdd881b44c0386bb06c700afc036aa970f01e241
lexie70 <116680625+lexie70@users.noreply.github.com>
2023-10-19 14:26:02 -0600

    style button

--- src/layouts/SearchUsers.jsx
+++ src/layouts/SearchUsers.jsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
 import { useDemoData } from "@mui/x-data-grid-generator";
+import { Box, Button, Divider } from "@mui/material";
+import "./../styles/search-users.css";
 
 const SearchUsers = () => {
   const { data } = useDemoData({
@@ -12,11 +13,18 @@ const SearchUsers = () => {
   });
 
   return (
-    <div>
-      <h3>SearchUsers</h3>
-      <div>
+    <div className="content-filter">
+      <h3 className="">Buscar Usuario</h3>
+      <div className="button-box-filter">
         <Link to={"/"}>
-          <button>Home</button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="medium"
+            hovered="medium"
+          >
+            Inicio
+          </Button>
         </Link>
       </div>
 
