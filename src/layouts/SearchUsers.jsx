7015080329dcdac26c5cd10f1c3afadadd2825e5
Juan Jose CS <jconejo@prides.net>
2023-10-19 10:33:38 -0600

    feature: Add table to Search Users

--- src/layouts/SearchUsers.jsx
+++ src/layouts/SearchUsers.jsx
@@ -1,5 +1,23 @@
+import * as React from 'react';
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+
+
 const SearchUsers = () => {
+
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 20,
+    maxColumns: 12,
+  });
+
+
+
+
+
   return (
     <div>
       <h3>SearchUsers</h3>
@@ -8,6 +26,20 @@ const SearchUsers = () => {
           <button>Home</button>
         </Link>
       </div>
+
+      <Box sx={{ height: 300, width: '100%' }}>
+      <DataGrid
+        {...data}
+        sx={{
+          boxShadow: 2,
+          border: 2,
+          borderColor: 'primary.light',
+          '& .MuiDataGrid-cell:hover': {
+            color: 'primary.main',
+          },
+        }}
+      />
+    </Box>
     </div>
   );
 };
